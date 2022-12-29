14ebbe955a46b00756e293fbc07b042e21cea0df
Gecko604 <alexkupec1247@gmail.com>
2022-12-29 13:08:44 -0600

    Saving progress. Needs to be albe to buy and sell weapons.

--- Assets/WeaponItemUI.cs
+++ Assets/WeaponItemUI.cs
@@ -5,30 +5,95 @@ using TMPro;
 
 public class WeaponItemUI : MonoBehaviour
 {
+    [Header("Weapon Info")]
     public string name;
-    public string price;
+    public int price;
 
+    [Header("Weapon Item")]
     public TextMeshProUGUI weaponNameLabel;
-    public TextMeshProUGUI weaponPriceLabel;
 
-    [Range(1,7)]
+    [Header("Weapon Showcase")]
+    public TextMeshProUGUI weaponNameDisplayLabel;
+    //public TextMeshProUGUI weaponPriceLabel;
+
+    [Header("Weapon Stats")]
+    [Range(0, 7)]
     public int damage;
-    [Range(1, 7)]
+    [Range(0, 7)]
     public int ammo;
-    [Range(1, 7)]
+    [Range(0, 7)]
     public int range;
-    [Range(1, 7)]
+    [Range(0, 7)]
     public int weight;
 
     
-    private void OnEnable()
+    private void Awake()
     {
-        
-    }
+        weaponNameLabel.text = name;
 
+        // Test
+        //DisplayWeapon();
+    }
 
     public void WeaponSelected()
     {
+        Debug.Log("button selected");
+        updateDisplayLabels();
+    }
 
+    private void updateDisplayLabels()
+    {
+        weaponNameDisplayLabel.text = name;
+        //weaponPriceLabel.text = $"${price}";
+
+        DisplayWeapon();
+    }
+
+    public void DisplayWeapon()
+    {
+        try
+        {
+            Shopkeeper keeper = GameObject.Find("ShopUI").GetComponent<Shopkeeper>();
+
+            keeper.NewItemToDisplay(this);
+
+        }
+        catch (System.Exception)
+        {
+
+            Debug.Log("ERROR: NO KEEPER FOUND IN SCENE");
+        }
+    }
+
+    public void BuyWeapon()
+    {
+        try
+        {
+            Shopkeeper keeper = GameObject.Find("ShopUI").GetComponent<Shopkeeper>();
+
+            keeper.BuyItem(this);
+
+        }
+        catch (System.Exception)
+        {
+
+            Debug.Log("ERROR: NO KEEPER FOUND IN SCENE");
+        }
+    }
+
+    public void SellWeapon()
+    {
+        try
+        {
+            Shopkeeper keeper = GameObject.Find("ShopUI").GetComponent<Shopkeeper>();
+
+            keeper.SellItem(this);
+
+        }
+        catch (System.Exception)
+        {
+
+            Debug.Log("ERROR: NO KEEPER FOUND IN SCENE");
+        }
     }
 }
